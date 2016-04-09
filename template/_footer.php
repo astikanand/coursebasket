489eec267bfbd1fe3cd0ef63dfeaa3a3b5863ff3
astikanand <astikanand@gmail.com>
2016-04-09 19:07:40 +0530

    template directory added with files _header.php and _footer.php

---
+++ template/_footer.php
@@ -0,0 +1,12 @@
+        </div>
+		<div class="cl">&#160;</div>
+	</div>
+</div>
+
+
+<div id="footer">
+	<div id="footer_in">
+		&copy; </div>
+</div>
+</body>
+</html>
---
+++ template/_header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title>coursebasket</title>
+	<meta name="description" content="Online Solution to India's Education Problem @ coursebasket" />
+	<meta name="description" content="Choose top universities, colleges and institutes for your degree, pg diploma, certificates, entrance coaching and school education courses online @ EduKart."/>
+    <meta name="keywords" content="degree, diploma, certificate course, entrance coaching, school education, k12, universities, colleges, institutes, pg diploma, online education in india, top colleges in india, top universities in india, online degree courses in india, correspondence courses, regular courses"/>
+	<meta http-equiv="imagetoolbar" content="no" />
+	<link href="/css/core.css" rel="stylesheet" type="text/css" />
+</head>
+
+
+<body>
+<div id="header">
+	<div id="header_in">
+		<h5><a href="/">Business name</a></h5>
+	</div>
+</div>
+<div id="outer">
+	<div id="wrapper">
+		<div id="left">
+			<h2>Categories</h2>
+			<ul id="navigation">
+				<li><a href="#">link</a></li>
+			</ul>		
+		</div>
+		<div id="right">
