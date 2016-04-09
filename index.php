4d6e5db45aecf788aec3bd75866b571dbadb1766
astikanand <astikanand@gmail.com>
2016-04-09 19:04:23 +0530

    Initial index.php added

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('includes/autoload.php');
+/*
+ * An object $core is created using Core() class and then
+ * run() method is executed
+ */
+$core = new Core();
+$core->run();
+
+?>
