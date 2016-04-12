01c6baf6d52c99aadf053229e93cf5adfd057569
astikanand <astikanand@gmail.com>
2016-04-12 21:06:43 +0530

    Catalogue.php modified

--- classes/Catalogue.php
+++ classes/Catalogue.php
@@ -8,18 +8,18 @@ class Catalogue extends Application {
 
 	private $_table = 'categories';
 	private $_table_2 = 'products';
+	private $_table_3 = 'institutes';
 	public $_path = 'media/catalogue/';
-	public static $_currency = '&pound;';
+	public static $_currency = '&#8377;';
 	
-	
-	
+
 	
 	/* This function is to fetch all the names from the categories table
 	 * it fetches from datatbase
 	 */
 	public function getCategories() {
 		$sql = "SELECT * FROM `{$this->_table}`
-				ORDER BY `name` ASC";
+				ORDER BY `id` ASC";
 		return $this->db->fetchAll($sql);
 	}
 	
@@ -48,6 +48,40 @@ class Catalogue extends Application {
 		return $this->db->fetchAll($sql);
 	}
 	
+	
+	
+	
+	/* Function to get specific product from database which is 
+	 * specified by passing $id parameter
+	 */
+	 public function getProduct($id) {
+		$sql = "SELECT * FROM `{$this->_table_2}`
+				WHERE `id` = '".$this->db->escape($id)."'";
+		return $this->db->fetchOne($sql);
+	}
+	
+	
+	
+	/* This function is to fetch all the names from the institutes table
+	 * it fetches from datatbase
+	 */
+	public function getInstitutes() {
+		$sql = "SELECT * FROM `{$this->_table_3}`
+				ORDER BY `id` ASC";
+		return $this->db->fetchAll($sql);
+	}
+	
+	
+	
+	
+	/* Function to get specific institute category from database which is 
+	 * specified by passing $id parameter
+	 */
+	public function getInstitute($id) {
+		$sql = "SELECT * FROM `{$this->_table_3}`
+				WHERE `id` = '".$this->db->escape($id)."'";
+		return $this->db->fetchOne($sql);
+	}
 	
 	
 }
